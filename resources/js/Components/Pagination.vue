58b10c62d2f03a1d311a83a3407e2647b5c0d963
Martín Calderón <elmcwl@gmail.com>
2024-10-20 01:11:45 -0700

    feat: add progress in pagination module

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,10 +1,34 @@
 <script setup>
+  import { router } from '@inertiajs/vue3';
+
   defineProps({
     data: {
       type: Object,
       required: true,
     },
   });
+  
+  // old way to redirect to other page
+  // const updatePageNumber = (link) => {
+  //   let pageNumber = link.url.split("=")[1];
+    
+  //   router.visit('/students?page=' + pageNumber, {
+  //       preserveScroll: true,
+  //     });
+  // };
+  
+  // this way to redirect to other page is better, because it will get only the page number in the url
+  const updatePageNumber = (link) => {
+
+    const url = new URL(link.url);
+    const params =  url.searchParams;
+
+    url.searchParams.set('page', params.get('page'));
+
+    router.visit(url.href,  {
+          preserveScroll: true,
+    });
+  }
   
 </script>
 
@@ -48,13 +72,17 @@
                             aria-label="Pagination"
                         >
                             <button
+                                @click.prevent="updatePageNumber(link)"
+                                v-for="(link, index) in data.meta.links"
+                                :key="index"
+                                :disabled="link.acive || !link.url"
                                 class="relative inline-flex items-center px-4 py-2 border text-sm font-medium"
                                 :class="{
-                                    'z-10 bg-indigo-50 border-indigo-500 text-indigo-600': true,
-                                    'bg-white border-gray-300 text-gray-500 hover:bg-gray-50': false,
+                                    'z-10 bg-indigo-50 border-indigo-500 text-indigo-600': link.active,
+                                    'bg-white border-gray-300 text-gray-500 hover:bg-gray-50': !link.active,
                                 }"
                             >
-                                <span>1</span>
+                                <span v-html="link.label"></span>
                             </button>
                         </nav>
                     </div>
