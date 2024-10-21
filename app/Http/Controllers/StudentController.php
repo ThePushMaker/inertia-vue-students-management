21f8d5b69c1cd112d0e2ce5d016a1e82ae912706
Martín Calderón <elmcwl@gmail.com>
2024-10-20 22:35:14 -0700

    feat: add create student method in student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ClassesResource;
 use App\Http\Resources\StudentResource;
+use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Http\Request;
 
@@ -13,5 +15,12 @@ class StudentController extends Controller
         $students = StudentResource::collection(Student::paginate(10));
         
         return inertia('Students/Index', compact('students'));
+    }
+    
+    public function create()
+    {
+        $classes = ClassesResource::collection(Classes::all());
+        
+        return inertia('Students/Create', compact('classes'));
     }
 }
